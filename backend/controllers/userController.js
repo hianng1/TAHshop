35a142a5a62970d8104ff12b38d2567cec8458d0
hianng1 <baidman3501@gmail.com>
2024-12-17 18:04:49 +0700

    18:56

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,28 @@ import asyncHandler from "../middlewares/asyncHandler.js";
 import User from "../models/userModel.js";
 
 const createUser = asyncHandler(async(req, res) =>{
-    res.send("hello");
+    const {username, email, password} = req.body;
+    if (!username || !email || !password){
+        throw new Error("pls fill all the inputs!!");
+    }
+    const userExists = await User.findOne({email});
+    if(userExists) res.status(400).send("User already exists");
+    const newUser = new User({username, email, password})
+
+    try {
+        await newUser.save();
+        res
+          .status(201)
+          .json({
+            _id: newUser._id,
+            username: newUser.username,
+            email: newUser.email,
+            isAdmin: newUser.isAdmin,
+          });
+    } catch (error) {
+        res.status(400)
+        throw new Error("Invalid user data!!");
+    }
 });
 
 export {createUser};
