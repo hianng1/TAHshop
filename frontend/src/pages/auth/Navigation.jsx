402f0305b27de85a1b700500e9ff2f1db69e3a0a
hianng1 <baidman3501@gmail.com>
2024-12-23 17:52:45 +0700

    2:03:00

--- frontend/src/pages/auth/Navigation.jsx
+++ frontend/src/pages/auth/Navigation.jsx
@@ -5,13 +5,18 @@ import {
   AiOutlineLogin,
   AiOutlineUserAdd,
   AiOutlineShoppingCart,
+  AiOutlineShop,
 } from "react-icons/ai";
 import { FaHeart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import "./Navigation.css";
+import { useSelector, useDispatch } from "react-redux";
+import { useLoginMutation } from "../../redux/api/usersApiSlice";
+import { logout } from "../../redux/features/auth/authSlice";
 
 const Navigation = () => {
+  const { userInfo } = useSelector((state) => state.auth);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [showSidebar, setshowSidebar] = useState(false);
   const toggleDropdown = () => {
@@ -23,6 +28,19 @@ const Navigation = () => {
   const closeSidebar = () => {
     setshowSidebar(false);
   };
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [logoutApiCall] = useLoginMutation();
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div
       style={{ zIndex: 999 }}
@@ -32,11 +50,61 @@ const Navigation = () => {
       id="navigation-container"
     >
       <div className="flex flex-col justify-center space-y-4">
-        <Link to='/' className="flex items-center transition-transform transform hover:translate-x-2">
-          <AiOutlineHome className="mr-2 mt-[3rem]" size={26}/>
-          <span className="hidden nav-item-name mt-[3rem]">Home</span>
+        <Link
+          to="/"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <AiOutlineHome className="mr-2 mt-[3rem]" size={26} />
+          <span className="hidden nav-item-name mt-[3rem]">Home</span>{" "}
+        </Link>
+        <Link
+          to="/shop"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <AiOutlineShop className="mr-2 mt-[3rem]" size={26} />
+          <span className="hidden nav-item-name mt-[3rem]">Shop</span>{" "}
+        </Link>
+        <Link
+          to="/cart"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <AiOutlineShoppingCart className="mr-2 mt-[3rem]" size={26} />
+          <span className="hidden nav-item-name mt-[3rem]">Cart</span>{" "}
+        </Link>
+        <Link
+          to="/favourite"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <FaHeart className="mr-2 mt-[3rem]" size={26} />
+          <span className="hidden nav-item-name mt-[3rem]">Favourite</span>{" "}
         </Link>
       </div>
+      <div className="relative">
+        <button
+          onClick={toggleDropdown}
+          className="flex items-center text-gray-8000 focus:outline-none"
+        >{userInfo ? <span className="text-white">{userInfo.username} </span> : (<></>)}</button>
+      </div>
+      <ul>
+        <li>
+          <Link
+            to="/login"
+            className="flex items-center transition-transform transform hover:translate-x-2"
+          >
+            <AiOutlineLogin className="mr-2 mt-[3rem]" size={26} />
+            <span className="hidden nav-item-name mt-[3rem]">Login</span>{" "}
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/register"
+            className="flex items-center transition-transform transform hover:translate-x-2"
+          >
+            <AiOutlineUserAdd className="mr-2 mt-[3rem]" size={26} />
+            <span className="hidden nav-item-name mt-[3rem]">Login</span>{" "}
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
