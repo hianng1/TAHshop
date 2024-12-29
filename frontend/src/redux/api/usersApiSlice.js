7c2569fdd8985e6d2a2ebd582d5cd27eb2876cfe
hianng1 <baidman3501@gmail.com>
2024-12-29 11:37:26 +0700

    done with user manament

--- frontend/src/redux/api/usersApiSlice.js
+++ frontend/src/redux/api/usersApiSlice.js
@@ -26,12 +26,50 @@ export const userApiSlice = apiSlice.injectEndpoints({
     profile: builder.mutation({
       query: (data) => ({
         url: `${USERS_URL}/profile`,
-        method:"PUT",
-        body : data,
-      })
-    })
+        method: "PUT",
+        body: data,
+      }),
+    }),
+    getUsers: builder.query({
+      query: () => ({
+        url: USERS_URL,
+      }),
+      providesTags: ["User"],
+      keepUnusedDataFor: 5,
+    }),
+
+    deleteUser: builder.mutation({
+      query: (userID) => ({
+        url: `${USERS_URL}/${userID}`,
+        method: "DELETE",
+      }),
+    }),
+
+    getUserDetails: builder.query({
+      query: (id) => ({
+        url: `${USERS_URL}/${id}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+
+    updateUser: builder.mutation({
+      query: ({ id, ...body }) => ({
+        url: `${USERS_URL}/${id}`, // Chèn ID của user vào URL
+        method: "PUT", // Hoặc PATCH nếu chỉ cập nhật một phần
+        body, // Gửi phần dữ liệu cần update (ngoài ID)
+      }),
+      invalidatesTags: ["User"], // Đảm bảo refetch lại dữ liệu sau khi update
+    }),
   }),
 });
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation ,useProfileMutation} =
-  userApiSlice;
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  useProfileMutation,
+  useGetUsersQuery,
+  useDeleteUserMutation,
+  useGetUserDetailsQuery,
+  useUpdateUserMutation,
+} = userApiSlice;
