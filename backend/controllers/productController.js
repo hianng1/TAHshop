c1e5a44434382c707dbbe35d836e4446e9af54ec
hianng1 <baidman3501@gmail.com>
2025-01-07 16:59:25 +0700

    done backend for products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,85 +1,195 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
-import Product from '../models/productModel.js'
+import Product from "../models/productModel.js";
 
 const addProduct = asyncHandler(async (req, res) => {
-    try {
-        const {name, description, price,category, quantity, brand} = req.fields
-        // console.log(name)
-        // console.log(description)
-        // console.log(price)
-        // console.log(category)
-        // console.log(quantity)
-        // console.log(brand)
-        switch (true) {
-            case !name:
-                return res.json({error: "Name is required!"})
-            case !brand:
-                return res.json({error: "brand is required!"})
-            case !description:
-                return res.json({error: "description is required!"})
-            case !price:
-                return res.json({error: "price is required!"})
-            case !category:
-                return res.json({error: "category is required!"})        
-            case !quantity:
-                return res.json({error: "quantity is required!"})   
-        }
-        const product = new Product({...req.fields});
-        await product.save();
-        res.json(product);
-    } catch (error) {
-        console.log(error)
-        res.status(400).json(error.message)
+  try {
+    const { name, description, price, category, quantity, brand } = req.fields;
+    // console.log(name)
+    // console.log(description)
+    // console.log(price)
+    // console.log(category)
+    // console.log(quantity)
+    // console.log(brand)
+    switch (true) {
+      case !name:
+        return res.json({ error: "Name is required!" });
+      case !brand:
+        return res.json({ error: "brand is required!" });
+      case !description:
+        return res.json({ error: "description is required!" });
+      case !price:
+        return res.json({ error: "price is required!" });
+      case !category:
+        return res.json({ error: "category is required!" });
+      case !quantity:
+        return res.json({ error: "quantity is required!" });
     }
+    const product = new Product({ ...req.fields });
+    await product.save();
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error.message);
+  }
 });
 
-const updateProduct = asyncHandler(async(req, res) => {
-    try {
-        const {name, description, price,category, quantity, brand} = req.fields
-        switch (true) {
-            case !name:
-                return res.json({error: "Name is required!"})
-            case !brand:
-                return res.json({error: "brand is required!"})
-            case !description:
-                return res.json({error: "description is required!"})
-            case !price:
-                return res.json({error: "price is required!"})
-            case !category:
-                return res.json({error: "category is required!"})        
-            case !quantity:
-                return res.json({error: "quantity is required!"})   
-        }
-        const product = await Product.findByIdAndUpdate(req.params.id, {...req.fields},{new: true})
-        await product.save();
-        res.json(product);
-    } catch (error) {
-        console.log(error)
-        res.status(400).json(error.message)
+const updateProduct = asyncHandler(async (req, res) => {
+  try {
+    const { name, description, price, category, quantity, brand } = req.fields;
+    switch (true) {
+      case !name:
+        return res.json({ error: "Name is required!" });
+      case !brand:
+        return res.json({ error: "brand is required!" });
+      case !description:
+        return res.json({ error: "description is required!" });
+      case !price:
+        return res.json({ error: "price is required!" });
+      case !category:
+        return res.json({ error: "category is required!" });
+      case !quantity:
+        return res.json({ error: "quantity is required!" });
     }
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      { ...req.fields },
+      { new: true }
+    );
+    await product.save();
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error.message);
+  }
 });
 
-const removeProduct = asyncHandler(async(req, res) => {
-    try {
-        const product = await Product.findByIdAndDelete(req.params.id);
-        res.json({message: "Delete success"});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({error: "Server error"})
+const removeProduct = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    res.json({ message: "Delete success" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+const fetchProducts = asyncHandler(async (req, res) => {
+  try {
+    const pageSize = 6;
+    const keyword = req.query.keyword
+      ? { name: { $regex: req.query.keyword, $options: "i" } }
+      : {};
+    const count = await Product.countDocuments({ ...keyword });
+    const products = await Product.find({ ...keyword }).limit(pageSize);
+    res.json({
+      products,
+      page: 1,
+      pages: Math.ceil(count / pageSize),
+      hasMore: false,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error!" });
+  }
+});
+
+const fetchProductById = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      return res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not found!");
     }
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ error: "Product not found!" });
+  }
+});
+
+const fetchAllProducts = asyncHandler(async (req, res) => {
+  try {
+    const products = await Product.find({})
+      .populate("category")
+      .limit(12)
+      .sort({ createAt: -1 });
+
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+const addProductReview = asyncHandler(async (req, res) => {
+  try {
+    const { rating, comment } = req.body;
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      const alrReviewed = product.reviews.find(
+        (r) => r.user.toString() === req.user._id.toString()
+      );
+
+      if (alrReviewed) {
+        res.status(400);
+        throw new Error("Product already reviewed");
+      }
+
+      const review = {
+        name: req.user.username,
+        rating: Number(rating),
+        comment,
+        user: req.user._id,
+      };
+
+      product.reviews.push(review); // Sửa 'review' thành 'reviews'
+      product.numReviews = product.reviews.length;
+
+      product.rating =
+        product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+        product.reviews.length;
+
+      await product.save();
+      res.status(201).json({ message: "Review added" });
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message }); // Sửa cấu trúc trả về lỗi
+  }
+});
+
+const fetchTopProduct = asyncHandler(async(req, res) => {
+  try {
+    const products = await Product.find({}).sort({rating: -1}).limit(4);
+    res.json(products)
+  } catch (error) {
+    console.log(error)
+    res.status(400).json(error.message)
+  }
 })
 
-const fetchProducts = asyncHandler(async(req, res) => {
-    try {
-        const pageSize = 6
-        const keyword = req.query.keyword ? {name: {$regex : req.query.keyword, $options: "i"}}
-        : {};
-        const count = await Product.countDocuments({...keyword})
-        const products = await Product.find({...keyword}).limit(pageSize)
-        res.json({products, page: 1, pages: Math.ceil(count / pageSize), hasMore: false})
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({error : "Server error!"})
-    }
+const fetchNewProduct = asyncHandler(async(req, res) => {
+  try {
+    const products = await Product.find().sort({_id:-1}).limit(5)
+    res.json(products)
+  } catch (error) {
+    console.log(error)
+    res.status(400).json(error.message)
+  }
 })
-export {addProduct, updateProduct ,removeProduct, fetchProducts};
+export {
+  addProduct,
+  updateProduct,
+  removeProduct,
+  fetchProducts,
+  fetchProductById,
+  fetchAllProducts,
+  addProductReview,
+  fetchTopProduct,
+  fetchNewProduct
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,19 +1,39 @@
 // import React from 'react'
-import express from "express"
-import formidable from "express-formidable"
-import checkId from '../middlewares/checkId.js'
-import { addProduct ,updateProduct ,removeProduct, fetchProducts} from "../controllers/productController.js";
+import express from "express";
+import formidable from "express-formidable";
+import checkId from "../middlewares/checkId.js";
+import {
+  addProduct,
+  updateProduct,
+  removeProduct,
+  fetchProducts,
+  fetchProductById,
+  fetchAllProducts,
+  addProductReview,
+  fetchTopProduct,
+  fetchNewProduct
+} from "../controllers/productController.js";
 const router = express.Router();
-import {authenticate, authorizedAdmin} from '../middlewares/authMiddleware.js'
+import {
+  authenticate,
+  authorizedAdmin,
+} from "../middlewares/authMiddleware.js";
 
+router
+  .route("/")
+  .get(fetchProducts)
+  .post(authenticate, authorizedAdmin, formidable(), addProduct);
 
-router.route('/')
-.get(fetchProducts)
-.post(authenticate, authorizedAdmin, formidable(), addProduct)
-
-
-router.route('/:id')
-.put(authenticate, authorizedAdmin, formidable(), updateProduct)
-.delete(authenticate, authorizedAdmin, removeProduct)
+router.get("/top", fetchTopProduct);
+router.route("/allproducts").get(fetchAllProducts);
+router.get('/new', fetchNewProduct)
+router
+  .route("/:id/reviews")
+  .post(authenticate, authorizedAdmin, checkId, addProductReview);
+router
+  .route("/:id")
+  .get(fetchProductById)
+  .put(authenticate, authorizedAdmin, formidable(), updateProduct)
+  .delete(authenticate, authorizedAdmin, removeProduct);
 
 export default router;
